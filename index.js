7958345c7c3bc680fbc1a736d03354586ec00a7f
Craig5117 <craig.bennett5117@gmail.com>
2020-11-27 12:42:04 -0500

    project title and description questions are functioning

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            }
+            else {
+              console.log('Please enter a title.');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project. (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          }
+          else {
+            console.log('Please enter a description.');
+            return false;
+          }
+        }
+      },
 
 ];
 
@@ -8,9 +37,15 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+const init = () => {
+    return inquirer.prompt(
+        questions
+    );
 
-}
+};
 
 // function call to initialize program
-init();
+init()
+.then(answers => {
+    console.log(answers);
+});
